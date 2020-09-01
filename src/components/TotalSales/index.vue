8148ff26b87bedceb63cfee7f01a3d35300f41a2
gyd1997 <gyd0008@163.com>
2020-09-01 23:01:17 +0800

    顶部核心组件抽取

---
+++ src/components/TotalSales/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <common-card
+      title="累计销售额"
+      value="￥ 32,039,165"
+    >
+      <template>
+        <div class="compare">
+          
+        </div>
+      </template>
+      <template v-slot:footer>
+          <span>昨日销售额</span>
+          <span class="money">￥ 30,000,000</span>
+      </template>
+    </common-card>
+  </div>
+</template>
+
+<script>
+import commonCardMixin from '@/utils/commonCardMixin'
+export default {
+  mixins: [commonCardMixin]
+}
+</script>
+
+<style lang="scss" scoped>
+.compare {
+  color: #666;
+  margin-top: 3px;
+  font-size: 12px;
+}
+.money {
+  margin-left: 5px;
+  color: #333;
+  font-weight: 700;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import './plugins/element.js'
 import echarts from 'echarts'
 
+import 'element-ui/lib/theme-chalk/index.css';
+
 Vue.config.productionTip = false
 Vue.prototype.$echarts = echarts
 
